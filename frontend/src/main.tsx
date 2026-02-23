6cf73c0f93e9116601e24a398ef05f34f6bdf90b
JOELNATHAN544 <wankojoelnathan@gmail.com>
2026-02-23 16:08:21 +0100

    refactor(app): remove ErrorBoundary from root

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,14 +1,11 @@
 import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
-import { ErrorBoundary } from './components/shared/ErrorBoundary.tsx';
 import './i18n/config.ts';
 import { Suspense } from 'react';
 
 createRoot(document.getElementById('root')!).render(
   <Suspense fallback="loading...">
-    <ErrorBoundary>
-      <App />
-    </ErrorBoundary>
+    <App />
   </Suspense>
 );
