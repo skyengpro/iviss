3acb4b22ee0b0b7287bc68d7b0efacdcbb81c63a
JOELNATHAN544 <wankojoelnathan@gmail.com>
2026-03-02 13:54:49 +0100

    feat(config): add JWT secret

--- iviss-backend/src/app_state.rs
+++ iviss-backend/src/app_state.rs
@@ -2,10 +2,14 @@ use crate::db::DbPool;
 
 pub struct AppState {
     pub db: DbPool,
+    pub jwt_secret: String,
 }
 
 impl AppState {
-    pub fn new(db_pool: DbPool) -> Self {
-        Self { db: db_pool }
+    pub fn new(db_pool: DbPool, jwt_secret: String) -> Self {
+        Self {
+            db: db_pool,
+            jwt_secret,
+        }
     }
 }
