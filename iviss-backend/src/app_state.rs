27906d4c932e9dec04f0b91fb96d1ac66a99a972
Rolland Ayonta <ayonta.lontsi@skyengpro.com>
2026-03-02 02:58:18 +0100

    feat: update AppState to include SmsProvider and modify main function for SMS integration

--- iviss-backend/src/app_state.rs
+++ iviss-backend/src/app_state.rs
@@ -1,15 +1,20 @@
 use crate::db::{redis::RedisPool, DbPool};
+use crate::services::sms_provider::SmsProvider;
+use std::sync::Arc;
 
+#[derive(Clone)]
 pub struct AppState {
     pub db: DbPool,
     pub redis: RedisPool,
+    pub sms_pvd: Arc<dyn SmsProvider>,
 }
 
 impl AppState {
-    pub fn new(db_pool: DbPool, redis_pool: RedisPool) -> Self {
+    pub fn new(db_pool: DbPool, redis_pool: RedisPool, sms_pvd: Arc<dyn SmsProvider>) -> Self {
         Self {
             db: db_pool,
             redis: redis_pool,
+            sms_pvd,
         }
     }
 }
