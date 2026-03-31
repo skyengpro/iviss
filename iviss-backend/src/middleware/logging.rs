5ead4953bfa8bf1386ee0812beae5e9b2a11bd1b
Rolland Ayonta <ayonta.lontsi@skyengpro.com>
2026-03-31 13:44:58 +0100

    refactor: clean up unused routes and remove logging middleware

--- iviss-backend/src/middleware/logging.rs
+++
@@ -1,6 +0,0 @@
-use axum::{extract::Request, middleware::Next, response::Response};
-
-pub async fn log_request(request: Request, next: Next) -> Response {
-    tracing::info!("Request: {} {}", request.method(), request.uri());
-    next.run(request).await
-}
