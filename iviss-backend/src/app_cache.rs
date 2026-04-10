56338ba757ec9e8f58410ef1180d41bc809b7584
Rolland Ayonta <ayonta.lontsi@skyengpro.com>
2026-04-10 10:08:35 +0100

    feat(cache): implement AppCache with OTP and rate limiting functionality

---
+++ iviss-backend/src/app_cache.rs
@@ -0,0 +1,59 @@
+use moka::future::Cache;
+use std::time::Duration;
+use uuid::Uuid;
+
+
+const OTP_TTL_SECS: u64        = 300;  // 5 min
+const RATE_LIMIT_TTL_SECS: u64 = 600;  // 10 min
+const NONCE_TTL_SECS: u64      = 60;   // 1 min
+const JTI_BLACKLIST_TTL_SECS: u64 = 180; // 3 min
+
+#[derive(Clone, Debug)]
+struct OtpEntry {
+    otp_hash: String,
+    attempts: u8,
+}
+#[derive(Clone)]
+struct AppCache {
+    /// Key: user_id (Uuid)
+    /// Valeur : OtpEntry { code_hash, attempts }
+    otp_store: Cache<Uuid, OtpEntry>,
+    /// Key: phone_number (String)
+    /// Number of request in the last RATE_LIMIT_TTL_SECS time window
+    rate_limit: Cache<String, u32>,
+    /// Key: device_id (Uuid)
+    /// Valeur : nonce base64 (String)
+    refresh_nonce: Cache<Uuid, String>,
+    /// Key: jti (String)
+    /// Value: ()
+    jti_blacklist: Cache<String, ()>,
+}
+
+impl AppCache {
+    pub fn new() -> Self {
+        Self {
+            otp_store: Cache::builder()
+                .max_capacity(10_000)
+                .time_to_live(Duration::from_secs(OTP_TTL_SECS))
+                .build(),
+            rate_limit: Cache::builder()
+                .max_capacity(30_000)
+                .time_to_live(Duration::from_secs(RATE_LIMIT_TTL_SECS))
+                .build(),
+            refresh_nonce: Cache::builder()
+                .max_capacity(10_000)
+                .time_to_live(Duration::from_secs(NONCE_TTL_SECS))
+                .build(),
+            jti_blacklist: Cache::builder()
+                .max_capacity(10_000)
+                .time_to_live(Duration::from_secs(JTI_BLACKLIST_TTL_SECS))
+                .build(),
+        }
+    }
+}
+
+impl Default for AppCache {
+    fn default() -> Self {
+        Self::new()
+    }
+}
