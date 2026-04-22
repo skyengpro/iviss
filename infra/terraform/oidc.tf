a8aa8ef091a5a495247d541b298aff250fcc2297
donemmanuelo <emmanuelodon943@gmail.com>
2026-04-22 18:59:03 +0100

    fix: restore aud claim and implement repository-level trust

--- infra/terraform/oidc.tf
+++ infra/terraform/oidc.tf
@@ -33,15 +33,12 @@ resource "aws_iam_role" "github_actions_deploy" {
           Federated = aws_iam_openid_connect_provider.github_actions.arn
           }
           Action = "sts:AssumeRoleWithWebIdentity"
-        Condition = {
-          StringLike = {
-              "token.actions.githubusercontent.com:sub" = [
-              "repo:skyengpro/iviss:ref:refs/heads/main",
-              "repo:skyengpro/iviss:ref:refs/heads/dev",
-              "repo:skyengpro/iviss:ref:refs/heads/aws-dev-test"
-            ]
+          Condition = {
+            StringLike = {
+              "token.actions.githubusercontent.com:aud" = "sts.amazonaws.com"
+              "token.actions.githubusercontent.com:sub" = "repo:skyengpro/iviss:*"
+            }
           }
-        }
       }
     ]
   })
