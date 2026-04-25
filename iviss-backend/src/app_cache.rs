f11c4470fc442408c200e53513fdc5bbea7ab8e1
Rolland Ayonta <ayonta.lontsi@skyengpro.com>
2026-04-25 17:03:28 +0100

    refactor: rename org_shift_hours to org_work_time in cache,

--- iviss-backend/src/app_cache.rs
+++ iviss-backend/src/app_cache.rs
@@ -55,8 +55,8 @@ pub struct AppCache {
     /// Value: ()
     pub jti_blacklist: Cache<String, ()>,
     /// Key: organization_id (Uuid)
-    /// Value: (shift_start_minute, shift_end_minute)
-    pub org_shift_hours: Cache<Uuid, (u32, u32)>,
+    /// Value: (start_work_time, end_work_time)
+    pub org_work_time: Cache<Uuid, (u32, u32)>,
 }
 
 impl AppCache {
@@ -78,7 +78,7 @@ impl AppCache {
                 .max_capacity(10_000)
                 .time_to_live(Duration::from_secs(JTI_BLACKLIST_TTL_SECS))
                 .build(),
-            org_shift_hours: Cache::builder().max_capacity(50).build(),
+            org_work_time: Cache::builder().max_capacity(50).build(),
         }
     }
 
